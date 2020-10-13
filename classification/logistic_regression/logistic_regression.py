01bd8509e786f2da5df823406fc2816300209edb
Jakub Stępniak <jakub@stpniak.org>
2020-10-13 21:59:46 +0200

    classification logistic regression

---
+++ classification/logistic_regression/logistic_regression.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+# Training the Logistic Regression model on Training set
+classifier = LogisticRegression(random_state=0)  # how far without parameters we are creating object
+classifier.fit(X_train, Y_train)
+
+"""
+# Predict a new result
+y_uno_pred = classifier.predict(sc.transform([[30, 8700]]))  # i have to remember to transform the data that i check if i did feature scaling earlier
+print(y_uno_pred)
+
+# Predict the Test set result
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), Y_test.reshape(len(Y_test), 1)), 1))  # we are creating matrix or two vectors to easier compare them
+
+# Making the Confusion Matrix
+# confusion matrix is 2D matrix (2 rows, 2 columns) and it will how as number of corrected predictions in previous point
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(Y_test, y_pred)
+print(cm)
+a_s = accuracy_score(Y_test, y_pred)  # return a value of accuracy (1 is the highest value)
+print(a_s)  # it returns how many corrected predictions we have in percentage
+
+# Visualising the Training set result
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set result
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_test), Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+"""
